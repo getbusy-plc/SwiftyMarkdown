b5fe25e2f537e55fb9df0b9d8abd64452f2e9803
Andrew Lord <andrew.lord@getbusy.com>
2023-09-07 13:18:03 +0100

    Remove example apps and playgrounds

--- Package.swift
+++ Package.swift
@@ -6,14 +6,14 @@ let package = Package(
     platforms: [
         .iOS(SupportedPlatform.IOSVersion.v11),
         .tvOS(SupportedPlatform.TVOSVersion.v11),
-		.macOS(.v10_12),
-		.watchOS(.v4)
+        .macOS(.v10_12),
+        .watchOS(.v4),
     ],
     products: [
         .library(name: "SwiftyMarkdown", targets: ["SwiftyMarkdown"]),
     ],
     targets: [
         .target(name: "SwiftyMarkdown"),
-		.testTarget(name: "SwiftyMarkdownTests", dependencies: ["SwiftyMarkdown"])
+        .testTarget(name: "SwiftyMarkdownTests", dependencies: ["SwiftyMarkdown"]),
     ]
 )
